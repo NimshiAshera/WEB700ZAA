bfa1d2b2b0babe21fafe82a19c06148a9c51aadf
NimshiAshera <38455210+NimshiAshera@users.noreply.github.com>
2024-10-11 21:08:24 -0400

    Commits to Assignment 03

---
+++ Assignments/Assignment03/web700-app/modules/collegeData.js
@@ -0,0 +1,96 @@
+// collegeData.js
+const fs = require('fs');
+const { resolve } = require('path');
+
+class Data {
+    constructor(students, courses) {
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/students.json', 'utf8', (err, studentDataFromFile) => {
+            if (err) {
+                reject("unable to read students.json");
+                return;
+            }
+            let studentData = JSON.parse(studentDataFromFile);
+
+            fs.readFile('./data/courses.json', 'utf8', (err, courseDataFromFile) => {
+                if (err) {
+                    reject("unable to read courses.json");
+                    return;
+                }
+                let courseData = JSON.parse(courseDataFromFile);
+                dataCollection = new Data(studentData, courseData);
+                resolve();
+            });
+        });
+    });
+}
+
+function getAllStudents() {
+    return new Promise((resolve, reject) => {
+        if (dataCollection.students.length > 0) {
+            resolve(dataCollection.students);
+        } else {
+            reject("no results returned");
+        }
+    });
+}
+
+function getTAs() {
+    return new Promise((resolve, reject) => {
+        let TAs = dataCollection.students.filter(student => student.TA === true);
+        if (TAs.length > 0) {
+            resolve(TAs);
+        } else {
+            reject("no results returned");
+        }
+    });
+}
+
+function getCourses() {
+    return new Promise((resolve, reject) => {
+        if (dataCollection.courses.length > 0) {
+            resolve(dataCollection.courses);
+        } else {
+            reject("no results returned");
+        }
+    });
+}
+
+function getStudnetsByCourse(course){
+    return new Promise((resolve, reject) => {
+        let studentsOfCourse = dataCollection.students.filter(student => student.course === course);
+        
+        if (studentsOfCourse.length > 0){
+            resolve(studentsOfCourse);
+        } else {
+            reject("no results returned");
+        }
+    });
+}
+
+function getStudnetByNum(num){
+    return new Promise((resolve, reject) => {
+        let student = dataCollection.students.find(student => student.studentNum === num);
+        if (student){
+            resolve(student);            
+        } else{
+            reject("no results returned");
+        }
+    });
+}
+
+// Export the functions
+module.exports = {
+    initialize,
+    getAllStudents,
+    getTAs,
+    getCourses
+};
